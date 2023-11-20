bed5126a31bae6d1121bd55d653d62050d714c21
sw5l <arnasandrijaitis@gmail.com>
2023-11-20 19:05:45 +0000

    updated html and external javascript with comments outlying plan

---
+++ externalIndexAndGame.js
@@ -0,0 +1,12 @@
+//click start to begin the game's main function
+
+//make an array with 9 values
+
+//loop a math.random function and assign a random number to each array value,
+// so i will end up with 9 random numbers.
+
+
+//all 9 buttons will be hidden by default, and then when pressed they will
+//display the appropriate image based on the mathematical value of the value
+
+//the user must get 3 matching images in a row to win the game, it is completely luck based.
