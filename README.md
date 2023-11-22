1b7303a9d82398feb003ad746fa8f69dbd52ded2
sw5l <arnasandrijaitis@gmail.com>
2023-11-22 12:21:51 +0000

    updated externalIndexAndGame.js to have basic code to outline my game idea as a frame

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # project
+ to make changes on git,
+make the change to the file, add a message with
+git commit -m "(message here)"
+if you use "git commit" to edit the message, do
+"rm -f ./.git/(filename).lock" to cancel
+
+finally do git push origin main
+to push the changes and make a commit.
--- externalIndexAndGame.js
+++ externalIndexAndGame.js
@@ -1,9 +1,27 @@
+//declarations, these will be used for later as baseline numbers or something, i'll figure it out
+let i = 0;
 //click start to begin the game's main function
 
+function startGame(){
+	
 //make an array with 9 values
+//i learned indepth about arrays from w3schools https://www.w3schools.com/js/js_arrays.asp
 
-//loop a math.random function and assign a random number to each array value,
+const buttons = [];
+
+//loop a math.random function and assign a random number to each array value, 
 // so i will end up with 9 random numbers.
+
+for (let i = 0; i < 9; i++) {
+buttons[i] = (Math.floor(Math.random() * 10));	
+};
+
+	alert(buttons);
+	alert(buttons[3]);
+};
+
+
+
 
 
 //all 9 buttons will be hidden by default, and then when pressed they will
